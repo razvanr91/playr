525ea72f4f63aec58ef547d1431ae592fef36cf7
razvanr91 <razvan.roman91@outlook.com>
2021-11-24 14:05:31 +0000

    Add control icons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle, faAngleDoubleLeft, faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Player() {
     return (
@@ -8,7 +10,11 @@ export default function Player() {
                 <input type="range" />
                 <p>end time</p>
             </div>
-            <div className="play-control"></div>
+            <div className="play-control">
+                <FontAwesomeIcon className="previousTrack" size="2x" icon={faAngleDoubleLeft} />
+                <FontAwesomeIcon className="play" size="2x" icon={faPlayCircle} />
+                <FontAwesomeIcon className="nextTrack" size="2x" icon={faAngleDoubleRight} />
+            </div>
         </div>
     );
 }
